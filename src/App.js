2508b1ca5479c86aa550e73387114030c877609c
yuhei022 <yuhei022@users.noreply.github.com>
2023-04-14 20:23:18 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. This message is from
-          develop branch.
+          develop branch. 2nd
         </p>
         <a
           className="App-link"
